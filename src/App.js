db6ada26c833af7804a07749d928d556be954e22
Santtu Taskinen <santtu-taskinen@outlook.com>
2020-02-05 13:11:14 +0200

    Basic logic implemented on Front-End side.

--- src/App.js
+++ src/App.js
@@ -7,34 +7,63 @@ class App extends Component {
   constructor(props) {
   super(props);
   this.state = {
-    points: 5,
-    game: 'Pay to Play'
+    points: 20,
+    game: 'Pay to Play',
+    counter: 0,
+    message: ''
   }
 }
 
 // Function to handle basic userside logic.
   playGame = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+      points: this.state.points - 1
+    });
     if (this.state.points <= 0) {
       this.setState({
         points: 20,
-        game: 'Pay to Play'
+        game: 'Pay to Play',
+        message: 'Welcome back'
       });
-    } else if (this.state.points === 1) {
+    } else if (this.state.points-1 === 0) {
       this.setState({
-        points: this.state.points - 1,
-        game: 'You have lost! Continue?'
+        message: 'Game Over',
+        game: 'Continue?'
       });
     } else {
-      this.setState({points: this.state.points - 1});
+      if ((this.state.counter+1)%500 === 0 && this.state.counter !== 0) {
+        this.setState({
+          message: 'Congratulations! You won 250 points',
+          points: this.state.points + 250
+      });
+      } else if ((this.state.counter+1)%100 === 0 && this.state.counter !== 0) {
+        this.setState({
+          message: 'Congratulations! You won 40 points',
+        points: this.state.points + 40
+      });
+      } else if ((this.state.counter+1)%10 === 0 && this.state.counter !== 0) {
+        this.setState({
+          message: 'Congratulations! You won 5 points',
+        points: this.state.points + 5
+      });
+      } else {
+        this.setState({
+          message: 'Sorry! No win this time',
+        });
+      }
     }
   }
   render() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          {this.state.points}
+  <h1>{this.state.message}</h1>
+      <h1>
+          Points: {this.state.points}
         </h1>
+        <h1>
+          Counter: {this.state.counter}</h1>
         
         <Button variant="contained" color="secondary" onClick={this.playGame}>
           {this.state.game}
